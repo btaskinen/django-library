63914b58ddd5407e0f1533b13c354d4ef95e7c21
Barbara Taskinen <barbara.taskinen@gmail.com>
2026-01-26 14:50:17 +0200

    Create model for genre

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
+"""Database models for the catalog application."""
 from django.db import models
+from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
+from django.db.models import UniqueConstraint # Constrains fields to unique values
+from django.db.models.functions import Lower # Returns lower cased value of field
 
-# Create your models here.
+class Genre(models.Model):
+    """Model representing a book genre"""
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+    )
+
+    def __str__(self) -> str:
+        """String for representing the Model object."""
+        return str(self.name)
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular genre instance."""
+        return reverse('genre-detail', args=[str(self.id)])
+
+    class Meta:
+        """Metadata for Genre model."""
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='genre_name_case_insensitive_unique',
+                violation_error_message="Genre already exists (case insensitive match)"
+            )
+        ]
