5d9dc1b98e7746491f2b773055ec7c6da0a76ecf
Barbara Taskinen <barbara.taskinen@gmail.com>
2026-01-29 10:37:45 +0200

    Further modify admin models
    - updated list view for AdminBook Instances
    - added inline listing to AdminAuthor and AdminBook models

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,21 +3,47 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
+class BookInline(admin.TabularInline):
+    """tabular inline class for books"""
+    model = Book
+    extra = 0
+
 @admin.register(Author)
 class AdminAuthor(admin.ModelAdmin):
     """Customizes the Django admin interface for Author objects."""
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+class BookInstanceInline(admin.TabularInline):
+    """tabular inline class for book instances"""
+    model = BookInstance
+    extra = 0
 @admin.register(Book)
 class AdminBook(admin.ModelAdmin):
     """Customizes the Django admin interface for Book objects."""
     list_display = ('title', 'authors', 'display_genre')
+
+    inlines = [BookInstanceInline]
 
 admin.site.register(Genre)
 
 @admin.register(BookInstance)
 class AdminBookInstance(admin.ModelAdmin):
     """Customizes the Django admin interface for BookInstance objects."""
+    list_display = ('book', 'status', 'due_back', 'id')
+
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
 
 admin.site.register(Language)
