f87ed54df1a3d9305008b768dcf0c96af1c58d8b
Barbara Taskinen <barbara.taskinen@gmail.com>
2026-02-18 09:49:05 +0200

    Create tests for author model

---
+++ catalog/tests/test_models.py
@@ -0,0 +1,48 @@
+"""Tests for models for the catalog app."""
+from django.test import TestCase
+from catalog.models import Author
+
+class AuthorModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Author.objects.create(first_name='Big', last_name='Bob')
+
+    def test_first_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEqual(field_label, 'first name')
+
+    def test_last_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('last_name').verbose_name
+        self.assertEqual(field_label, 'last name')
+
+    def test_date_of_birth_label(self):
+        author = Author.objects.get(id=1)
+        field_label =author._meta.get_field('date_of_birth').verbose_name
+        self.assertEqual(field_label, 'Born')
+
+    def test_date_of_death_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEqual(field_label, 'Died')
+
+    def test_first_name_max_lenght(self):
+        author = Author.objects.get(id=1)
+        max_lenght = author._meta.get_field('first_name').max_length
+        self.assertEqual(max_lenght, 100)
+
+    def test_last_name_max_lenght(self):
+        author = Author.objects.get(id=1)
+        max_lenght = author._meta.get_field('last_name').max_length
+        self.assertEqual(max_lenght, 100)
+
+    def test_object_name_is_last_name_comma_first_name(self):
+        author = Author.objects.get(id=1)
+        expexted_object_name = f'{author.last_name}, {author.first_name}'
+        self.assertEqual(str(author), expexted_object_name)
+
+    def get_absolute_url(self):
+        author = Author.objects.get(id=1)
+        self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
+
